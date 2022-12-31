1f62ca93cd55c44132a1e80a63710987dbc956c5
MahimaSanketh <mahimadevinfo@gmail.com>
2022-12-31 10:49:57 +0530

    Words searcher

---
+++ words.py
@@ -0,0 +1,24 @@
+# words search programe
+# small demo programe
+
+#! this is  dictionary basic concept
+
+print('********finde words********')
+word = {
+    'a': [' abase', ' aberration', 'abhor', 'abject', 'abrasive', ' abstain', 'abstract', ' abundant', 'accentuate', ' acclimate'],
+    'b': ['beaf', 'black', 'bird', 'bat', 'ban', 'bank', 'balm', 'bark', 'birth', 'berth', 'bask', 'basket', 'bingo', 'binge', 'bloom',],
+    'c': ['cat', 'cab', 'car', 'canvas', 'cups'],
+    'd': ['dog', 'dust', 'dusbin', 'duck'],
+    'e': ['english', 'englend', 'eat']
+}
+
+
+user = input('enter words \t:')
+
+x = word[user]
+
+print(x)
+
+
+# ****** please feedback my code ************
+# * my email --->: mahimadevinfo@gmail.com
